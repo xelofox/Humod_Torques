2a173998dd26365e791528d1062afc5360ddbd4a
xelofox <38127459+xelofox@users.noreply.github.com>
2020-07-10 15:58:21 +0200

    Update TorquesByNE.m

--- Newton-Euler/TorquesByNE.m
+++ Newton-Euler/TorquesByNE.m
@@ -35,7 +35,7 @@ d_footL_AJL_c=[foot_R.comX;foot_R.comY;foot_R.comZ]*1e-3;
 d_head_LNJ_c=[head.comX;head.comY;head.comZ]*1e-3;
 
 %inter-joint distances
-d_LLJ_ULJ=abdomen.relativePosition.LLJ*1e-3;
+-d_LLJ_ULJ=abdomen.relativePosition.LLJ*1e-3;
 d_ULJ_LNJ=[0;thorax.segmentLengthY;0]*1e-3; %valeur probablement pas tout à fait exact mais non trouvée dans la doc
 d_LNJ_SJR=thorax.relativePosition.SJ_R*1e-3;
 d_LNJ_SJL=thorax.relativePosition.SJ_L*1e-3;
